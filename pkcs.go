d9fbe966b83523b96e5dc26b840445cfb5832cf5
Mads Freek <freek@wayf.dk>
2025-10-28 14:30:51 +0100

    Added support for RSA keys

--- pkcs.go
+++ pkcs.go
@@ -1,7 +1,7 @@
 package sshca
 
 import (
-    "fmt"
+	"fmt"
 	"io"
 	"log"
 
@@ -10,13 +10,18 @@ import (
 )
 
 const (
-	CKM_EDDSA = 0x80000c03 // not in pkcs11 yet
+	CKM_EDDSA = 0x80000c03 // Thales kryptoki - not in pkcs11 yet
+    //CKM_EDDSA = 0x1057 // softHSM
 )
 
 var (
-	p        *pkcs11.Ctx
-	tokenMap = map[string]uint{}
-	sessions chan pkcs11.SessionHandle
+	p             *pkcs11.Ctx
+	tokenMap      = map[string]uint{}
+	sessions      chan pkcs11.SessionHandle
+	pkcs11MechMap = map[string]uint{
+		"ssh-ed25519": CKM_EDDSA,
+		"ssh-rsa":     pkcs11.CKM_SHA256_RSA_PKCS,
+	}
 )
 
 // Signer
@@ -26,7 +31,7 @@ type hsmSigner struct {
 }
 
 func InitPKCS11(pin string) {
-    sessions = make(chan pkcs11.SessionHandle, Config.NoOfSessions)
+	sessions = make(chan pkcs11.SessionHandle, Config.NoOfSessions)
 	p = pkcs11.New(Config.Cryptokilib)
 	if p == nil {
 		log.Fatal("No cryptoki lib available")
@@ -56,12 +61,12 @@ func InitPKCS11(pin string) {
 		if e != nil {
 			log.Fatalf("Failed to open session: %s\n", e.Error())
 		}
-        fmt.Println("session", session, e)
+		fmt.Println("session", session, e)
 		e = p.Login(session, pkcs11.CKU_USER, pin)
 		if e != nil {
 			log.Printf("Failed to login to session: %s\n", e.Error())
 		}
-        fmt.Println("login",sessions,  e)
+		fmt.Println("login", sessions, e)
 		sessions <- session
 	}
 	fmt.Println("pkcs11.end of init")
@@ -69,10 +74,10 @@ func InitPKCS11(pin string) {
 }
 
 func findPrivatekey(label string) (pkcs11.ObjectHandle, bool) {
-    if len(sessions) == 0 {
-        log.Println("no HSM sessions available - HSM keys can't be used")
-        return 0, false
-    }
+	if len(sessions) == 0 {
+		log.Println("no HSM sessions available - HSM keys can't be used")
+		return 0, false
+	}
 	session := <-sessions
 	defer func() { sessions <- session }()
 	template := []*pkcs11.Attribute{pkcs11.NewAttribute(pkcs11.CKA_LABEL, label), pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PRIVATE_KEY)}
@@ -95,14 +100,17 @@ func findPrivatekey(label string) (pkcs11.ObjectHandle, bool) {
 func (signer hsmSigner) Sign(rand io.Reader, digest []byte) (signature *ssh.Signature, err error) {
 	session := <-sessions
 	defer func() { sessions <- session }()
-	err = p.SignInit(session, []*pkcs11.Mechanism{pkcs11.NewMechanism(CKM_EDDSA, nil)}, signer.priv)
-	log.Println("signinit", err)
+	format := signer.PublicKey().Type()
+	err = p.SignInit(session, []*pkcs11.Mechanism{pkcs11.NewMechanism(pkcs11MechMap[format], nil)}, signer.priv)
+	if err != nil {
+		log.Fatalf("failed to signinit: %s\n", err)
+	}
 	sig, err := p.Sign(session, digest)
 	if err != nil {
 		log.Fatalf("failed to sign: %s\n", err)
 	}
 	return &ssh.Signature{
-		Format: signer.PublicKey().Type(),
+		Format: format,
 		Blob:   sig,
 	}, err
 }
